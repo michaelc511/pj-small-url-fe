6810a800fe2f0d7a0bf9526a9f63825e5cbe59d8
michaelc511 <michaelc511@gmail.com>
2024-03-05 19:55:22 -0800

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-// import { useEffect, useState } from "react" 
 import { NewURLForm } from "./components/NewURLForm"
 import "./styles.css"
 
 function App() {
-  // const [count, setCount] = useState(0)
 
   return (
     <>
